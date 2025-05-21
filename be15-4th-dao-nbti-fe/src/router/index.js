c2305623fbb29e458b2d7094a1b49472a01c58b5
Taein5415 <kskk5050@naver.com>
2025-05-21 15:38:43 +0900

    [init]

--- be15-4th-dao-nbti-fe/src/router/index.js
+++ be15-4th-dao-nbti-fe/src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import mainRoutes from '@/features/main/router.js'
+import LayoutDefault from "@/components/layout/LayoutDefault.vue";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        //...
-        //나중에 다른 기능과 관련된 routes들을 추가하면 된다.
+        {
+            path: '/',
+            component: LayoutDefault,
+            children: [
+                ...mainRoutes,
+            ]
+        }
     ]
 })
 
