37c94d6353b7f183849b0417146898aa0dda28d1
Taein5415 <kskk5050@naver.com>
2025-05-23 02:19:43 +0900

    [feat #22] api 및 경로 추가

--- be15-4th-dao-nbti-fe/src/features/user/router.js
+++ be15-4th-dao-nbti-fe/src/features/user/router.js
@@ -1,5 +1,7 @@
 import LoginView from "@/features/user/views/LoginView.vue";
 import SignupView from "@/features/user/views/SignupView.vue";
+import FindPaswordView from "@/features/user/views/FindPaswordView.vue";
+import ResetPasswordView from "@/features/user/views/ResetPasswordView.vue";
 
 
 export const userRoutes = [
@@ -12,5 +14,15 @@ export const userRoutes = [
         path: '/signup',
         name: 'SignupView',
         component: SignupView
+    },
+    {
+        path: '/find-password',
+        name: 'FindPassword',
+        component: FindPaswordView
+    },
+    {
+        path: '/reset-password',
+        name: 'ResetPassword',
+        component: ResetPasswordView
     }
 ]
