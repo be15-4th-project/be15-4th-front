c4dffe67c788563a478e145d0244ba5c3d44fea8
splguyjr <splguyjr@naver.com>
2025-05-22 17:01:19 +0900

    [feat] 메인페이지 - 학습 리스트 컴포넌트 구현, 난이도 모달 구현, 비로그인 상태처리 구현

--- be15-4th-dao-nbti-fe/src/features/main/views/MainView.vue
+++ be15-4th-dao-nbti-fe/src/features/main/views/MainView.vue
@@ -1,10 +1,12 @@
 <script setup>
+import StudyCategoryListFrame from "@/features/main/components/StudyCategoryListFrame.vue";
 
 import TestStart from "@/features/main/components/TestStart.vue";
 
 </script>
 
 <template>
+  <StudyCategoryListFrame></StudyCategoryListFrame>
   <TestStart/>
 </template>
 
